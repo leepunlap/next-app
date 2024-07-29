d91de70ab001f42717ce3fd442a8536285f19419
Bernard Lee <bernardlee@hkctech.com>
2024-07-29 14:51:46 +0800

    Add motion state

--- app/mtr/stall.tsx
+++ app/mtr/stall.tsx
@@ -9,8 +9,11 @@ export default function Stall({ area, id }: { area: string, id: string }) {
   var ndx: number = parseInt(id) - 1;
   var myStall: StallStruct = (area === 'male') ? stalls.male[ndx] : (area === 'female') ? stalls.female[ndx] : (area === 'accessible') ? stalls.accessible[ndx] : stalls.baby[ndx];
 
-  var className = "stallState badge "
-  className += (myStall.state === 'unknown') ? "bg-secondary" : (myStall.state === 'occupied') ? "bg-danger" : (myStall.state === 'idle') ? "bg-success" : "bg-primary";
+  var classNameStallState = "stallState badge "
+  var classNameMotionState = "motionState badge "
+  classNameStallState += (myStall.state === 'unknown') ? "bg-secondary" : (myStall.state === 'occupied') ? "bg-danger" : (myStall.state === 'vacant') ? "bg-success" : "bg-primary";
+  classNameMotionState += (myStall.motion === 'unknown') ? "bg-secondary" : (myStall.motion === 'idle') ? "bg-dark" : (myStall.motion === 'motion') ? "bg-success" : "bg-primary";
+
   return (
     <div className="stall">
       <div className="stallText">
@@ -19,8 +22,11 @@ export default function Stall({ area, id }: { area: string, id: string }) {
       <div>
         <Image className="stallImg" src={imgStall} alt="" />
       </div>
-      <div className={className}>
+      <div className={classNameStallState}>
         {myStall.state}
+      </div>
+      <div className={classNameMotionState}>
+        {myStall.motion}
       </div>
     </div>
   );
--- app/mtr/zustand.tsx
+++ app/mtr/zustand.tsx
@@ -11,6 +11,7 @@ export type StallStruct = {
   door: string;
   pir: string;
   state: string;
+  motion: string;
 }
 
 export type StallsStruct = {
@@ -33,9 +34,9 @@ export const toiletInfo = create<CartStore>((set, get) => ({
         if (deviceId === x.door) {
           if (message.length == 36) {
             var bDoorOpen = (message[35] == '0') ? false : true;
-            var sDoorStatus = bDoorOpen ? "occupied" : "idle";
+            var sDoorStatus = bDoorOpen ? "occupied" : "vacant";
             x.state = sDoorStatus;
-            console.log(messageCnt, deviceId, 'door', x.name, bDoorOpen);
+            console.log(messageCnt, deviceId, 'door', x.name, message, message.length, sDoorStatus);
           } else {
             console.log(messageCnt, deviceId, 'door', x.name, message, message.length, "error");
           }
@@ -43,13 +44,12 @@ export const toiletInfo = create<CartStore>((set, get) => ({
         if (deviceId === x.pir) {
           if (message.length == 34) {
             var bMotion = (message[27] == '0') ? false : true;
-            var sMotionStatus = bMotion ? "occupied" : "idle";
-            x.state = sDoorStatus;
-            console.log(messageCnt, deviceId, 'door', x.name, bDoorOpen);
+            var sMotionStatus = bMotion ? "motion" : "idle";
+            x.motion = sMotionStatus;
+            console.log(messageCnt, deviceId, 'pir', x.name, message, message.length, sMotionStatus);
           } else {
-            console.log(messageCnt, deviceId, 'door', x.name, message, message.length, "error");
+            console.log(messageCnt, deviceId, 'pir', x.name, message, message.length, "error");
           }
-          console.log(messageCnt, deviceId, 'pir', x.name, message, message.length, message[29]);
         }
       });
     }
@@ -59,26 +59,26 @@ export const toiletInfo = create<CartStore>((set, get) => ({
   },
   stalls: {
     male: [
-      { name: "M1", door: "D1859533", pir: "C5291775", state: "unknown" },
-      { name: "M2", door: "C5076557", pir: "D2283443", state: "unknown" },
-      { name: "M3", door: "C5076407", pir: "C5272107", state: "unknown" }
+      { name: "M1", door: "D1859533", pir: "C5291775", state: "unknown", motion: "unknown" },
+      { name: "M2", door: "C5076557", pir: "D2283443", state: "unknown", motion: "unknown" },
+      { name: "M3", door: "C5076407", pir: "C5272107", state: "unknown", motion: "unknown" }
     ],
     female: [
-      { name: "F1", door: "C5080400", pir: "C5295202", state: "unknown" },
-      { name: "F2", door: "D1862081", pir: "D2224163", state: "unknown" },
-      { name: "F3", door: "D1862325", pir: "C5295135", state: "unknown" },
-      { name: "F4", door: "D1862225", pir: "D2249931", state: "unknown" },
-      { name: "F5", door: "D1862213", pir: "D2279188", state: "unknown" },
-      { name: "F6", door: "D1859835", pir: "D2248025", state: "unknown" },
-      { name: "F7", door: "D1859986", pir: "D2290841", state: "unknown" },
-      { name: "F8", door: "D1859613", pir: "D2278925", state: "unknown" },
-      { name: "F9", door: "D1866373", pir: "C5279751", state: "unknown" }
+      { name: "F1", door: "C5080400", pir: "C5295202", state: "unknown", motion: "unknown" },
+      { name: "F2", door: "D1862081", pir: "D2224163", state: "unknown", motion: "unknown" },
+      { name: "F3", door: "D1862325", pir: "C5295135", state: "unknown", motion: "unknown" },
+      { name: "F4", door: "D1862225", pir: "D2249931", state: "unknown", motion: "unknown" },
+      { name: "F5", door: "D1862213", pir: "D2279188", state: "unknown", motion: "unknown" },
+      { name: "F6", door: "D1859835", pir: "D2248025", state: "unknown", motion: "unknown" },
+      { name: "F7", door: "D1859986", pir: "D2290841", state: "unknown", motion: "unknown" },
+      { name: "F8", door: "D1859613", pir: "D2278925", state: "unknown", motion: "unknown" },
+      { name: "F9", door: "D1866373", pir: "C5279751", state: "unknown", motion: "unknown" }
     ],
     accessible: [
-      { name: "A1", door: "D1866373", pir: "C5279751", state: "unknown" }
+      { name: "A1", door: "D1866373", pir: "C5279751", state: "unknown", motion: "unknown" }
     ],
     baby: [
-      { name: "B1", door: "D1866373", pir: "C5279751", state: "unknown" }
+      { name: "B1", door: "D1866373", pir: "C5279751", state: "unknown", motion: "unknown" }
     ],
   }
 }));
